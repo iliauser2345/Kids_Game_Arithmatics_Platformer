96163144890548d738011350bd11c82ca5900732
Ilia <mihlya1222@gmail.com>
2026-03-18 14:45:33 +0100

    ASSETS+STATES+CONST

---
+++ script/src/Constants.js
@@ -0,0 +1,44 @@
+export const KEYS ={
+    
+    _LWD: "A",
+    _RWD: "D",
+    _JMP: "Space",
+    _DGE: "Alt",
+    _AUX: "Shift", // auxiliary key for additional functions like sprint (Shift+A/D) etc.
+    _ATK:  0, //LMB
+    _ACT: "E",
+    _PAU: "P",
+    _DRP: "Q",
+    _INV: "I"
+
+}
+
+export const PlayerStates ={
+
+    _IDLE: "idle",
+    _WALK: "walk",
+    _RUN: "run",
+    _JUMP: "jump",
+    _INTERACT: "interact",
+    _ATTACK1: "attack1",
+    _ATTACK2: "attack2",
+    _ATTACK3: "attack3",
+    _ATTACK4: "attack4",
+    _DEATH: "death",
+    _HURT: "hurt"
+}
+export const NonPlayerStates ={
+
+    _NPCIDLE: "idle",
+    _NPCWALK: "walk",
+    _NPCRUN: "run",
+    _NPCJUMP: "jump",
+    _NPCATK: "attack",
+    _NPCHURT: "hurt",
+    _NPCDEATH: "death"
+}
+export const EnviromentStates ={
+    
+    _ENVIDLE: "idle",
+    _ENVINTERACTED: "interacted"
+}
