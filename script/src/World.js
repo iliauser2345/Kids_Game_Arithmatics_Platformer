6cba2e7341dd669e8cd0d735cb5c49dddcf6ba72
Ilia <mihlya1222@gmail.com>
2026-03-18 13:45:55 +0100

    setup

--- script/src/World.js
+++ script/src/World.js
@@ -1,10 +1,24 @@
 export class World{
 
+    
     constructor(){
+
+        this.backgrImg="";
+        this.tileImg="";
+        this.enemy;
 
     }
     // methods
     GenerateWorld(){
+
+
+
+
+
+
+
+
+
         console.log("worldgenned");
     }
 
