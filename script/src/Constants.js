ca7265fb19ceba0174d5c9e12dc2235412de4efe
sfeikens <s.feikens@sudent.alfa-college.nl>
2026-03-29 16:37:02 +0200

    Constants updated to available sprites, moved PlayerAnimations to Constants(as they are, constant)

--- script/src/Constants.js
+++ script/src/Constants.js
@@ -18,15 +18,13 @@ export const PlayerStates ={
     _IDLE: "idle",
     _WALK: "walk",
     _RUN: "run",
-    _JUMP: "jump",
-    _FALL: "fall",
-    _INTERACT: "interact",
     _ATTACK1: "attack1",
     _ATTACK2: "attack2",
     _ATTACK3: "attack3",
     _ATTACK4: "attack4",
-    _DEATH: "death",
-    _HURT: "hurt"
+    _JUMP: "jump",
+    _HURT: "hurt",
+    _DEATH: "death"
 }
 export const NonPlayerStates ={
 
@@ -49,4 +47,39 @@ export const EnviromentStates ={
 export const PlayerSize ={
     _WIDTH: 125,
     _HEIGHT: 130
+}
+
+/*****************************************
+ * Allows you to input the location of   *
+ * the correct image of the sprite sheet *
+ *****************************************/
+
+function getSpriteLoc(amountOfFrames, spritesheet_row) {
+    let returnArr = [];
+    for (let i = 0; i < amountOfFrames; i++) {
+        returnArr[i] = {
+            x: PlayerSize._WIDTH * i,
+            y: PlayerSize._HEIGHT * spritesheet_row
+        };
+    }
+    return returnArr;
+}
+
+/*********************************************
+ * Set locations of the sprites and how wide *
+ *********************************************/
+
+export const PlayerAnimations = {
+
+    "Idle":    { loc: getSpriteLoc(6, 0) },
+    "Walk":    { loc: getSpriteLoc(8, 1) },
+    "Run":     { loc: getSpriteLoc(7, 2) },
+    "Attack1": { loc: getSpriteLoc(5, 3) },
+    "Attack2": { loc: getSpriteLoc(2, 4) },
+    "Attack3": { loc: getSpriteLoc(5, 5) },
+    "Attack4": { loc: getSpriteLoc(5, 6) },
+    "Jump":    { loc: getSpriteLoc(6, 7) },
+    "Hurt":    { loc: getSpriteLoc(3, 8) },
+    "Death":   { loc: getSpriteLoc(4, 9) }
+
 }
